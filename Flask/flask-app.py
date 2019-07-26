26cb5e079b844710c9b395956050b54328f80647
Victor Chanil Park <ncksdlf92@gmail.com>
2019-07-26 13:53:29 +1200

    Add Flask

---
+++ Flask/flask-app.py
@@ -0,0 +1,21 @@
+from flask import Flask, escape, request, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
